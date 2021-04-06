ca583c47e3f92fe0608b3d86d7ede3fdf702039f
Дима булатов <dabulatov@edu.hse.ru>
2021-04-06 22:59:17 +0300

    test(s)

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,8 +3,23 @@
 #include <string>
 #include "train.h"
 
-TEST(SecondTest, test1) {
+TEST(FirstTest, test1) {
+  Train tr3(10);
+  EXPECT_EQ(10, tr3.getLen());
+}
+
+TEST(SecondTest, test2) {
   Train tr3(10);
   tr3.addCage(1);
   EXPECT_EQ(11, tr3.getLen());
+}
+
+TEST(ThirdTest, test3) {
+  Train tr3(10);
+  Cage* cg = tr3.getLast();
+  if (cg->next == tr3.getFirst()) {
+    EXPECT_EQ(1, 1);
+  } else {
+    EXPECT_EQ(0, 1);
+  }
 }
